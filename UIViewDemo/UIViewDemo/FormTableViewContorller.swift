9cb5669343456c324257193e656bfbfe228c9bdf
bhyn <bhyn9785@gmail.com>
2025-03-17 09:23:10 +0900

    chore: Improve table view layout and stack view setup

--- UIViewDemo/UIViewDemo/FormTableViewContorller.swift
+++ UIViewDemo/UIViewDemo/FormTableViewContorller.swift
@@ -5,4 +5,65 @@
 //  Created by 강보현 on 3/13/25.
 //
 
-import Foundation
+import UIKit
+
+class FormTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.separatorColor = .black
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        switch section {
+        case 0:
+            return 1
+        case 3:
+            return 3
+        default: return 2
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 테이블 뷰 셀을 재사용 하거나 새로 생성
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+
+        var config = UIListContentConfiguration.subtitleCell()
+        config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+        cell.contentConfiguration = config
+        cell.backgroundColor = .systemGray
+
+        return cell
+      }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+          return 100
+        case 1:
+          return 200
+        default:
+          return 50
+        }
+
+      }
+
+    }
+
+    #Preview {
+      FormTableViewController(style: .insetGrouped)
+    }
